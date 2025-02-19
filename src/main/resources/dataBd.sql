666e245a6fc865e4ae6c95a31659f3f66b24c54d
COSTINCIANU Gheorghina <gheorghina.costincianu@sfr.fr>
2025-02-19 10:12:27 +0100

    bdd supprimer

--- src/main/resources/dataBd.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE livre (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) UNIQUE NOT NULL,
-    `description` VARCHAR(255) NOT NULL,
-    date_publication DATE(50) NOT NULL
-);
